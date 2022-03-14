f0702ecaa38b5c6e77f65e59305e5ae53196100d
unknown <rafaelcerq91@gmail.com>
2022-03-13 22:36:35 -0300

    Show Score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,7 +22,7 @@ function Listing() {
     });
      
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
         .then(response => {
             const data = response.data as MoviePage;
             setPage(data);
@@ -39,7 +39,7 @@ function Listing() {
 
                     { page.content.map(movie => (
                         <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                            <MovieCard  movie = { movie } />
+                            <MovieCard  movie={movie} />
                         </div>
                     ))}
 
